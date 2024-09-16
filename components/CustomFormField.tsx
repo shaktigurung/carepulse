5be31fb32b786dd917f981975b20f27cf153bb09
Shakti Gurung <Shakti.Gurung@optus.com.au>
2024-09-16 16:37:10 +1000

    register form personal information section completed

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -14,6 +14,8 @@ import Image from "next/image";
 import 'react-phone-number-input/style.css'
 import PhoneInput from 'react-phone-number-input'
 import { E164Number } from 'libphonenumber-js/core';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 interface CustomProps {
     control: Control<any>,
@@ -31,7 +33,7 @@ interface CustomProps {
 }
 
 const RenderField = ({field, props}: { field:any; props: CustomProps}) => {
-    const { fieldType, iconSrc, iconAlt, placeholder } = props;
+    const { fieldType, iconSrc, iconAlt, placeholder, showTimeSelect, dateFormat, renderSkeleton } = props;
     switch (fieldType) {
         case FormFieldType.INPUT:
             return(
@@ -68,6 +70,30 @@ const RenderField = ({field, props}: { field:any; props: CustomProps}) => {
                     />
                 </FormControl>
             )
+        case FormFieldType.DATE_PICKER:
+            return(
+                <div className="flex rounded-md border border-dark-500 bg-dark-400">
+                <Image
+                  src="/assets/icons/calendar.svg"
+                  height={24}
+                  width={24}
+                  alt="user"
+                  className="ml-2"
+                />
+                <FormControl>
+                  <DatePicker
+                    showTimeSelect={showTimeSelect ?? false}
+                    selected={field.value}
+                    onChange={(date: Date) => field.onChange(date)}
+                    timeInputLabel="Time:"
+                    dateFormat={dateFormat ?? "MM/dd/yyyy"}
+                    wrapperClassName="date-picker"
+                  />
+                </FormControl>
+              </div>
+            )
+        case FormFieldType.SKELETON:
+            return renderSkeleton ? renderSkeleton(field) : null;
         default:
             break;
     }
