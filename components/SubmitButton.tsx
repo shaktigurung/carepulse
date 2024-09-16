e729f47de57a93305c1d88373792e36b0b72e211
Shakti Gurung <Shakti.Gurung@optus.com.au>
2024-09-16 15:08:29 +1000

    appwrite configured for application

--- components/SubmitButton.tsx
+++ components/SubmitButton.tsx
@@ -13,7 +13,7 @@ const SubmitButton = ({ isLoading, className, children }): ButtonProps => {
         { isLoading ? (
             <div className="flex items-center gap-4">
                 <Image 
-                    src="/assets/icons/spinner.svg"
+                    src="/assets/icons/loader.svg"
                     alt="loader"
                     width={24}
                     height={24}
